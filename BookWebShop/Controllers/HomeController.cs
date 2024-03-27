b98e746cff11efd6c801fe5c1bf3eb3e98dc18a6
karlafehir <fehirkarla@gmai.com>
2024-03-27 19:43:07 +0100

    Show products on home page, added product detail page

--- BookWebShop/Controllers/HomeController.cs
+++ BookWebShop/Controllers/HomeController.cs
@@ -2,27 +2,37 @@ using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 using BookWebShop.Models.Models;
 using BookWebShop.Models.Models;
+using BookWebShop.DataAccess.Repository.IRepository;
 
 namespace BookWebShop.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IUnitOfWork _unitOfWork;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
         {
             _logger = logger;
+            _unitOfWork = unitOfWork;
         }
 
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Product> productList = _unitOfWork.Product.GetAll(includeProperties: "Category").ToList();
+            return View(productList);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public IActionResult Details(int productId)
+        {
+            Product product = _unitOfWork.Product.Get(p=> p.Id == productId, includeProperties: "Category");
+            return View(product);
         }
     }
 }
